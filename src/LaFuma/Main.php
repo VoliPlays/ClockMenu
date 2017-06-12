bf5a278077d8832fb736e572f74c3ff2827e81e0
SuperKali <danychannel18@gmail.com>
2017-06-13 00:52:44 +0200

    Add Tile Chest

--- src/LaFuma/Main.php
+++ src/LaFuma/Main.php
@@ -10,8 +10,17 @@ use pocketmine\item\Item;
 use pocketmine\Level;
 use pocketmine\block\Block;
 use pocketmine\event\Listener;
+use pocketmine\inventory\Inventory;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\nbt\tags\IntArrayTag;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Chest;
+use pocketmine\tile\Tile;
 
 class Main extends PluginBase implements Listener{
 	
@@ -30,67 +39,27 @@ class Main extends PluginBase implements Listener{
 			$player->getInventory()->setItem(4, $item1);
 		
 	}
-	
-	public function onInteract(PlayerInteractEvent $event1){
+	public function sendChestInventory(Player $player){
+		$nbt = new CompoundTag('', [
+		new StringTag('id', Tile::CHEST),
+                new IntTag('§l§bSolarDinasty§', 1),
+                new IntTag('x', floor($player->x)),
+                new IntTag('y', floor($player->y) - 4),
+                new IntTag('z', floor($player->z))
+        ]);
 		
-		$player = $event1->getPlayer();
-		$item = $player->getInventory()->getItemInHand();
-		if($item->getCustomName() == "§l§bSolarDinasty§r"){
-			$player->getInventory()->clearAll();
-			//Info
-			$item2 = Item::get(Item::SLIMEBALL, 12,1);
-			$item2->setCustomName("§l§6Info§r");
-			$player->getInventory()->setItem(4,$item2);
-			//KitPvP
-			$item3 = Item::get(Item::DIAMOND_SWORD, 11,1);
-			$item3->setCustomName("§l§6KitPvP§r");
-			$player->getInventory()->setItem(5,$item3);
-			//Prison
-			$item4 = Item::get(Item::DIAMOND_PICKAXE, 10,1);
-			$item4->setCustomName("§l§6Prison§r");
-			$player->getInventory()->setItem(3,$item4);
-			//Back 1
-			$item1 = Item::get(Item::STICK, 14,1);
-			$item1->setCustomName("§4§lBack§r");
-			$player->getInventory()->setItem(8,$item1);
-			//Back 2
-			$item5 = Item::get(Item::STICK,9,1);
-			$item5->setCustomName("§l§4Back§r");
-			$player->getInventory()->setItem(0,$item5);
-			//Ignore This
-			//$item6 = Item::get(Item::WOOL,8,1);
-			//$item6->setCustomName("§l§cIgnore this§r");
-			//$player->getInventory()->setItem(2,$item6);
-			//Ignore This2
-			//$item7 = Item::get(Item::WOOL,7,1);
-			//$item7->setCustomName("§l§cIgnore this§r");
-			//$player->getInventory()->setItem(1,$item7);
-			//Ignore This3
-			//$item8 = Item::get(Item::WOOL,6,1);
-			//$item8->setCustomName("§l§cIgnore this§r");
-			//$player->getInventory()->setItem(6,$item8);
-			//Ignore This4
-			//$item9 = Item::get(Item::WOOL,5,1);
-			//$item9->setCustomName("§l§cIgnore this§r");
-			//$player->getInventory()->setItem(7,$item9);
-			
-}else if($item->getCustomName() == "§4§lBack§r"){
+        $tile = Tile::createTile('Chest', $player->getLevel(), $nbt);
+        $block = Block::get(Block::CHEST);
+        $block->x = floor($tile->x);
+        $block->y = floor($tile->y);
+        $block->z = floor($tile->z);
+        $block->level = $tile->getLevel();
+        $block->level->sendBlocks([$player], [$block]);
+        $inventory = $tile->getInventory();
 	$player->getInventory()->clearAll();
-	$item1 = Item::get(Item::CLOCK);
-    $item1->setCustomName("§l§bSolarDinasty§r");
-	$player->getInventory()->setItem(4, $item1);
-	
-  }else if($item->getCustomName() == "§l§4Back§r"){
-  	$player->getInventory()->clearAll();
-      $item1 = Item::get(Item::CLOCK);
-      $item1->setCustomName("§l§bSolarDinasty§r");
-      $player->getInventory()->setItem(4,$item1);
-      
-}else if($item->getCustomName() == "§l§6KitPvP§r"){
-$player->teleport($this->getServer()->getLevelByName("KitPvP")->getSafeSpawn());
-
-}else if($item->getCustomName() == "§l§6Prison§r"){
-$player->teleport($this->getServer()->getLevelByName("Prison")->getSafeSpawn());
-        }
-    }
- }
+	$test = Item::get(276, 0, 1);
+        $test->setCustomName("test");
+	$tile->getInventory()->setItem(0, $test);
+	$player->addWindow($inventory);
+	}
+}
